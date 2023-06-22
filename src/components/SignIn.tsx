63cb5637738bb56030eb20d4a3c64764b14e73b7
Sheehan Shams <s2shams@uwaterloo.ca>
2023-06-22 01:31:07 -0400

    google button added

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,6 @@
 import { Icons } from "./icons"
+import Link from "next/link"
+import UserAuthForm from "./UserAuthForm"
 
 const SignIn = () => {
   return <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px}">
@@ -9,6 +11,16 @@ const SignIn = () => {
                 </h1>
                 <p className="text-sm max-w-xs mx-auto">
                     By continuing, you are setting up a Ditto account and agreeing to our User Agreement and Privacy Policy
+                </p>
+
+                <UserAuthForm />
+
+                <p className="px-8 text-center text-sm text-zinc-700">
+                    New to Ditto?{' '}
+                    <Link href='/sign-up' 
+                          className='hover:text-zinc-800 text-sm underline underline-offset-4'>
+                        Sign Up
+                    </Link>
                 </p>
             </div>
         </div>
