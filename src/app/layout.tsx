e888276cde8e161c4d01a0ef416e11a703a765d0
Sheehan Shams <s2shams@uwaterloo.ca>
2023-06-23 19:35:57 -0400

    Added error notificaiton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import {Inter} from "next/font/google"
 import Navbar from "@/components/Navbar"
+import { Toaster } from '@/components/ui/Toaster'
 
 export const metadata = {
   title: 'Ditto',
@@ -26,6 +27,8 @@ export default function RootLayout({
           {children}
         </div>
         
+
+        <Toaster />
       </body>
     </html>
   )
